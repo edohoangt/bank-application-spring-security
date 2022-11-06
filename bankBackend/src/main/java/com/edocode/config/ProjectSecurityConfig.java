c1fea0f2d749d921799f64a7af475ebe92b4eb21
Hoang Tran <descience.thh10@gmail.com>
2022-11-06 15:11:02 +0700

    define and manage users

--- bankBackend/src/main/java/com/edocode/config/ProjectSecurityConfig.java
+++ bankBackend/src/main/java/com/edocode/config/ProjectSecurityConfig.java
@@ -3,6 +3,8 @@ package com.edocode.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -10,14 +12,22 @@ public class ProjectSecurityConfig {
 	
 	@Bean
 	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests()
-//			.requestMatchers().authenticated()
+		http.csrf().disable().authorizeHttpRequests()
 			.antMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
-//			.requestMatchers().permitAll()
-			.antMatchers("/notices", "/contact").permitAll()
+			.antMatchers("/notices", "/contact", "/register").permitAll()
 		.and().formLogin()
 		.and().httpBasic();
 		return http.build();
+	}
+	
+//	@Bean
+//	public UserDetailsService userDetailsService(DataSource dataSource) {
+//		 return new JdbcUserDetailsManager(dataSource);
+//	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
 	}
 	
 }
