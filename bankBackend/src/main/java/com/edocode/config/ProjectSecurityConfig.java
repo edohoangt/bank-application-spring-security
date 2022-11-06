3fe8a9e6fda1299650537e52ae00a449e0cd7fdc
Hoang Tran <descience.thh10@gmail.com>
2022-11-06 23:51:10 +0700

    apply BCryptPasswordEncoder to securely store password

--- bankBackend/src/main/java/com/edocode/config/ProjectSecurityConfig.java
+++ bankBackend/src/main/java/com/edocode/config/ProjectSecurityConfig.java
@@ -3,7 +3,7 @@ package com.edocode.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -20,14 +20,9 @@ public class ProjectSecurityConfig {
 		return http.build();
 	}
 	
-//	@Bean
-//	public UserDetailsService userDetailsService(DataSource dataSource) {
-//		 return new JdbcUserDetailsManager(dataSource);
-//	}
-	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
+		return new BCryptPasswordEncoder();
 	}
 	
 }
--- bankBackend/src/main/java/com/edocode/controller/LoginController.java
+++ bankBackend/src/main/java/com/edocode/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.edocode.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,13 +15,18 @@ import com.edocode.repository.CustomerRepository;
 public class LoginController {
 
 	@Autowired
-	CustomerRepository customerRepository;
+	private CustomerRepository customerRepository;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 	
 	@PostMapping("/register")
     public ResponseEntity<String> registerUser(@RequestBody Customer customer) {
         Customer savedCustomer = null;
         ResponseEntity response = null;
         try {
+        	String hashedPwd = passwordEncoder.encode(customer.getPwd());
+        	customer.setPwd(hashedPwd);
             savedCustomer = customerRepository.save(customer);
             if (savedCustomer.getId() > 0) {
                 response = ResponseEntity
